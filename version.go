392fcfa6545362b6357136d46c6f8ef0fff73a5c
nene209 <819569+nene209@users.noreply.github.com>
2021-02-05 19:03:31 -0800

    Fix so can use ldflags to set variables outside of this package  - Changing version -> Sha since it needs to be capitalized now  - Capitalizing Build

--- version.go
+++ version.go
@@ -4,12 +4,15 @@ var (
 	// Used for compile time versioning - to set properly, ensure to run
 	// the go install/build command like the following:
 	// go install -ldflags "-X github.com/Skyrin/go-lib/lib.version=local -X github.com/Skyrin/go-lib/lib.build=infinite"
-	version string
-	build   string
+
+	// Sha the commit sha
+	Sha string
+	// Build the build number
+	Build string
 )
 
 // Version returns the version/build
 // path to this in order for it to be usable.
 func Version() (string, string) {
-	return version, build
+	return Sha, Build
 }
