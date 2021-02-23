dc0fa036017f0785bb32a9696b7c143f2d7fa9cb
sagramont <sagramont@sagramont-mbp.local>
2021-02-22 19:52:03 -0700

    Add SQL errors

---
+++ sql/sql_errors.go
@@ -0,0 +1,23 @@
+package sql
+
+import (
+	"errors"
+
+	gle "github.com/Skyrin/go-lib/errors"
+	"github.com/lib/pq"
+)
+
+const (
+	// PQErr23505UniqueViolation Postgres code for unique violation
+	PQErr23505UniqueViolation = "23505"
+)
+
+// IsPQError checks if the passed error is the specified Postgres error code
+func IsPQError(err error, errorCode string) bool {
+	var pqerr *pq.Error
+	if ee := gle.AsExtendedError(err); ee != nil {
+		return ee.AsError(&pqerr) && string(pqerr.Code) == errorCode
+	}
+
+	return errors.As(err, &pqerr) && string(pqerr.Code) == errorCode
+}
