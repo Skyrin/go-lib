fa0611f29c417f5486a26e74e0f5d82e8f665406
sagramont <sagramont@sagramont-mbp.local>
2021-10-02 00:53:24 -0700

    Add sync that pushes to algolia after scanning the item

--- algolia/algolia_client.go
+++ algolia/algolia_client.go
@@ -4,14 +4,11 @@ package algolia
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/Skyrin/go-lib/algolia/model"
-	"github.com/Skyrin/go-lib/algolia/sqlmodel"
 	"github.com/Skyrin/go-lib/e"
 	"github.com/Skyrin/go-lib/sql"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
-	"github.com/rs/zerolog/log"
 )
 
 // Algolia handler for pushing index updates to Algolia
@@ -65,54 +62,8 @@ func (alg *Algolia) Delete(objectID string) (err error) {
 
 // Sync process to send all 'pending' and 'failed' records to algolia
 func (alg *Algolia) Sync(db *sql.Connection) (err error) {
-	// Get all items that are pending to be synced to algolia
-	status := []string{model.AlgoliaSyncStatusPending, model.AlgoliaSyncStatusFailed}
-	asList, _, err := sqlmodel.AlgoliaSyncGetByStatus(db, status, nil)
-	if err != nil {
-		return e.Wrap(err, e.Code0505, "01")
-	}
-
-	// Begin a transaction
-	if err := db.Begin(); err != nil {
-		return e.Wrap(err, e.Code0505, "02")
-	}
-	defer db.RollbackIfInTxn()
-
-	count := 0
-
-	// Loop and push to algolia
-	var wg sync.WaitGroup
-	for _, item := range asList {
-		count++
-		wg.Add(1)
-		go func(item *model.AlgoliaSync) {
-			defer wg.Done()
-
-			if err = alg.Push(item); err != nil {
-				log.Warn().Err(err).Msg(fmt.Sprintf("%s%s", e.Code0505, "03"))
-
-				// Change status to failed for that item
-				if err := sqlmodel.AlgoliaSyncStatusUpdate(db, item.ID,
-					model.AlgoliaSyncStatusFailed); err != nil {
-					log.Warn().Err(err).Msg(fmt.Sprintf("%s%s", e.Code0505, "04"))
-				}
-
-				return
-			}
-
-			// Change status to complete for that item
-			if err := sqlmodel.AlgoliaSyncStatusUpdate(db, item.ID,
-				model.AlgoliaSyncStatusComplete); err != nil {
-				log.Warn().Err(err).Msg(fmt.Sprintf("%s%s", e.Code0505, "05"))
-			}
-		}(item)
-	}
-
-	wg.Wait()
-
-	// Commit
-	if err := db.Commit(); err != nil {
-		return e.Wrap(err, e.Code0505, "06")
+	if err := ProccessItemsToPushToAlgolia(db, alg); err != nil {
+		return e.Wrap(err, e.Code0509, "01")
 	}
 
 	return nil
