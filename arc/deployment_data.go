0b11fa96c5fa0a33a334cbba6e29a6a8c9ddd9e9
nene209 <819569+nene209@users.noreply.github.com>
2022-01-14 02:40:16 +0000

    SKYR-2383 Simplify to handle only auth/saving data

--- arc/deployment_data.go
+++ arc/deployment_data.go
@@ -2,6 +2,7 @@ package arc
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -10,139 +11,126 @@ import (
 
 	"github.com/Skyrin/go-lib/arc/model"
 	"github.com/Skyrin/go-lib/arc/sqlmodel"
+	"github.com/Skyrin/go-lib/e"
 	"github.com/Skyrin/go-lib/sql"
-	"github.com/rs/zerolog/log"
 )
 
-// HTTPDataHandler handler for deployment data events
-type HTTPDataHandler struct {
-	Err          error
-	db           *sql.Connection
-	client       *Client
-	credentialID int
-	// loginURI     string // The path that will handle login
-	publishURI string // The path that will accept published data
+// DataHandler handler for deployment data events
+type DataHandler struct {
+	Err    error
+	db     *sql.Connection
+	client *Client
 
-	// Temp for testing
+	// local cache for token authorization - might replace with db entry somewhere
 	token       string
 	tokenExpiry int64
 }
 
-// err sets the error (so caller can see if there was one) then responds with
-// an http error
-func (hdh *HTTPDataHandler) err(w http.ResponseWriter, err error, msg string, code int) {
-	hdh.Err = err
-	http.Error(w, msg, code)
-}
+// NewDataHandler creates a new Data Handler for saving arc data events into the arc_data table
+func NewDataHandler(db *sql.Connection, client *Client) (adh *DataHandler) {
 
-// NewHTTPDataHandler creates a new HTTP Data Handler
-// The publishURI defines the path that will accept published arc data and save it
-// for processing into the application
-func NewHTTPDataHandler(db *sql.Connection, client *Client,
-	credentialID int, publishURI string) (adh *HTTPDataHandler) {
-
-	return &HTTPDataHandler{
-		db:           db,
-		client:       client,
-		credentialID: credentialID,
-		publishURI:   publishURI,
+	return &DataHandler{
+		db:     db,
+		client: client,
 	}
 }
 
-// ServeHTTP handles the http request
-func (hdh *HTTPDataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case hdh.publishURI:
-		hdh.handlePublish(w, r)
-	default:
-		hdh.err(w, nil, "URI not recognized", http.StatusBadRequest)
+// auth authorizes the request by looking for a valid bearer token in the authorization
+// header and either validating against the last cached token/expiry or the arc deployment
+// If it successfully validates against the arc deployment, it will cache the token/expiry
+// for future validations until the token expires or a new token is sent.
+func (dh *DataHandler) auth(r *http.Request) (authorized bool, msg string) {
+	// Verify the method is "POST"
+	if r.Method != http.MethodPost {
+		return false, "Must be a 'Post' request"
 	}
-}
-
-// auth authorizes the request, looking for a valid bearer token
-func (hdh *HTTPDataHandler) auth(w http.ResponseWriter, r *http.Request) (
-	authorized bool) {
 
 	a := r.Header.Get("Authorization")
 	if len(a) == 0 {
-		hdh.err(w, nil, "'Authorization' header missing", http.StatusBadRequest)
-		return false
+		return false, "'Authorization' header missing"
 	}
 
 	if !strings.HasPrefix(a, "Bearer ") {
-		hdh.err(w, nil, "invalid 'Authorization' type, expecting bearer token", http.StatusBadRequest)
-		return false
+		return false, "invalid 'Authorization' type, expecting bearer token"
 	}
 
 	t := a[7:]
 
 	// Validate the token
-	// TODO: lookup token/expiry in arc_config cache
-	cachedToken := hdh.token
-	cachedTokenExpiry := hdh.tokenExpiry
+	// TODO: store/lookup token/expiry in arc_config cache?
+	cachedToken := dh.token
+	cachedTokenExpiry := dh.tokenExpiry
 
 	// If token matches and hasn't expired, then return true
-	if cachedToken == t && cachedTokenExpiry < time.Now().Unix() {
-		return true
+	if cachedToken == t && cachedTokenExpiry > time.Now().Unix() {
+		return true, ""
 	}
 
 	// Attempt to validate the token
-	gui, err := hdh.client.GrantUserinfo(t)
+	gui, err := dh.client.GrantUserinfo(t)
 	if err != nil {
-		hdh.err(w, nil, "Unauthorized", http.StatusUnauthorized)
 		// Going to ignore the error as it should just indicate the token
 		// is invalid
-		return false
+		return false, e.MsgUnauthorized
 	}
 
+	// Ensure the type code is "app" as only the arc system can create an oauth2 token
+	// for an "app" user
 	if gui.TypeCode != "app" {
-		hdh.err(w, nil, "Unauthorized", http.StatusUnauthorized)
-		// TODO: need to validate the user?
-		return false
+		// TODO: need further validation?
+		return false, e.MsgUnauthorized
 	}
 
 	// Cache the token/expiry
-	// TODO: save to arc_config
-	hdh.token = t
+	// TODO: save to db in something like arc_config table?
+	dh.token = t
 	tokenExpiry, err := strconv.Atoi(r.Header.Get("ArcTokenExpiry"))
 	if err != nil {
 		tokenExpiry = 0
 	}
-	hdh.tokenExpiry = int64(tokenExpiry)
+	dh.tokenExpiry = int64(tokenExpiry)
 
-	return true
+	return true, ""
 }
 
-func (hdh *HTTPDataHandler) handlePublish(w http.ResponseWriter, r *http.Request) {
-	// Authorize the request
-	if !hdh.auth(w, r) {
-		return
+// Publish attempts to save the published data. It will first authorize the call and if
+// authorized, will save the data into the arc_data table. If it returns an error, then
+// something bad happened and the calling method should log the error. If it returns
+// anything but an empty message, then something was wrong with the call
+func (dh *DataHandler) Publish(r *http.Request) (msg string, code int, err error) {
+
+	authorized, msg := dh.auth(r)
+	if !authorized {
+		if msg == e.MsgUnauthorized {
+			return e.MsgUnauthorized, http.StatusUnauthorized, nil
+		}
+
+		// If it isn't an unauthorized message, then it was a bad request
+		return msg, http.StatusBadRequest, nil
 	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		hdh.err(w, err, "invalid body", http.StatusBadRequest)
-		return
+		return "invalid body", http.StatusBadRequest, nil
 	}
 
 	d := &model.Data{}
 	if err := json.Unmarshal([]byte(body), d); err != nil {
-		hdh.err(w, err, err.Error(), http.StatusBadRequest)
-		return
+		return err.Error(), http.StatusBadRequest, nil
 	}
 
 	if !d.IsValidAppCode() {
-		hdh.err(w, nil, "invalid app code specified", http.StatusBadRequest)
-		return
+		return "invalid app code specified", http.StatusBadRequest, nil
 	}
 
 	if !d.IsValidType() {
-		hdh.err(w, nil, "invalid type specified", http.StatusBadRequest)
-		return
+		return "invalid type specified", http.StatusBadRequest, nil
 	}
 
-	if err := sqlmodel.DataUpsert(hdh.db, d); err != nil {
-		hdh.err(w, err, "failed to upsert record", http.StatusBadRequest)
-		return
+	if err := sqlmodel.DataUpsert(dh.db, d); err != nil {
+		return e.MsgUnknownInternalServerError, http.StatusBadGateway,
+			e.Wrap(err, e.Code0415, "01", fmt.Sprintf("data: %s", body))
 	}
+
+	return "", http.StatusOK, nil
 }
